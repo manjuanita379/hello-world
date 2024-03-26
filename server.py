8f51613385d17753585bb6659563b2cefa20612c
manjuanita379 <145435792+manjuanita379@users.noreply.github.com>
2024-03-26 18:14:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    result = MESSAGE.encode("utf-8")
+    result = MESSAGE.encode("hello world")
     return result
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=PORT)
+    app.run(debug=True, host="54.151.2.1", port=8000)
